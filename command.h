2d4ed725ba2cfdd195c4ac025d9bce53d5075b22
Emad78 <mehdimohajeri78@gmail.com>
2019-05-13 00:15:12 +0430

    add set

--- command.h
+++ command.h
@@ -3,10 +3,33 @@
 
 #include "refrence.h"
 
+#define USERNAME "username"
+#define PASSWORD "password"
+#define AGE "age"
+#define EMAIL "email"
+#define PUBLISHER "publisher"
+
+#define NAME "name"
+#define YEAR "year"
+#define LENGTH "length"
+#define PRICE "price"
+#define SUMMARY "summary"
+#define DIRECTOR "director"
+
+#define MAX_YEAR "max_year"
+#define MIN_YEAR "min_year"
+#define MIN_RATE "min_rate"
+
+
 class Command
 {
 private:
-	map<string,string> key_value;
+	map<string,string> person_info;
+	map<string,string> film_info;
+	map<string,string> search_info;	
+	int method;
+	int request;
+
 
 public:
 	command();

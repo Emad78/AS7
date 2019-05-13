39c9ecc4056f26a3f8fddc5130ea1df207ca0d83
Emad78 <mehdimohajeri78@gmail.com>
2019-05-13 21:56:30 +0430

    create

---
+++ bad_request.h
@@ -0,0 +1,13 @@
+#ifndef BAD_REQUEST_H
+#define BAD_REQUEST_H
+
+#include "refrence.h"
+
+class Bad_request : public exception
+{
+public:
+	virtual const char* what() const throw()
+	{return "Bad Request"};
+};
+
+#endif

a81fdf9388d9d38f15e584ae8be826b673886a75
Emad78 <mehdimohajeri78@gmail.com>
2019-05-13 21:56:38 +0430

    create

---
+++ not_found.h
@@ -0,0 +1,13 @@
+#ifndef NOT_FOUND_H
+#define NOT_FOUND_H
+
+#include "refrence.h"
+
+class Not_found : public exception
+{
+public:
+	virtual const char* what() const throw()
+	{return "Not Found"};
+};
+
+#endif

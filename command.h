40e90d1765de75e5651d494c87771f07fba6b935
Emad78 <mehdimohajeri78@gmail.com>
2019-05-13 21:39:32 +0430

    change fild

--- command.h
+++ command.h
@@ -3,34 +3,14 @@
 
 #include "refrence.h"
 
-#define USERNAME "username"
-#define PASSWORD "password"
-#define AGE "age"
-#define EMAIL "email"
-#define PUBLISHER "publisher"
-
-#define NAME "name"
-#define YEAR "year"
-#define LENGTH "length"
-#define PRICE "price"
-#define SUMMARY "summary"
-#define DIRECTOR "director"
-
-#define MAX_YEAR "max_year"
-#define MIN_YEAR "min_year"
-#define MIN_RATE "min_rate"
-
-
 class Command
 {
 private:
-	map<string,string> info;
-	int method;
-	int request;
+	Input input;
 public:
 	command();
 	void set();
-	void input(string line);
+	Input get_input(string line);
 	string seprate_word(string &sentence,char delimiter);
 	friend class Person;
 	friend class System;

0e71b69bc0314339a6eaf72965fa92e13e0f94af
Emad78 <mehdimohajeri78@gmail.com>
2019-05-13 21:46:33 +0430

    add constructor

---
+++ system.cpp
@@ -0,0 +1,18 @@
+#include "system.h"
+
+System::System()
+{
+	system_money = 0;
+	command = new Command();
+	now_user = NULL;
+}
+
+System::~System()
+{
+	for(int i = films.size() - 1; i >= 0; i--)
+		delete films[i];
+	for(int i = users.size() - 1; i >= 0; i--)
+		delete users[i];
+	delete command;
+	delete now_user;
+}

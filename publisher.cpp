0d517a06a60aed99408bfd41a83cb55c55a010a3
Emad78 <mehdimohajeri78@gmail.com>
2019-05-14 23:13:53 +0430

    create add_my_film

--- publisher.cpp
+++ publisher.cpp
@@ -6,3 +6,8 @@ Publisher::publisher(Input input, int _id)
 	is_publisher = true;
 	followers.clear();
 }
+
+Publisher::add_my_film(Film* new_film)
+{
+	my_films.push_back(new_film);
+}
--- publisher.h
+++ publisher.h
@@ -8,8 +8,10 @@ class Publisher : public Person
 {
 private:
 	vector<Person*> followers;
+	vector<Film*> my_films;
 public:
 	Publisher(Input input, int _id);
+	void add_my_film(Film* new_film);
 	~Publisher();
 	
 };

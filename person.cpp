22fe391d8eb49eab7bae197ce651cfd1044850c4
Emad78 <mehdimohajeri78@gmail.com>
2019-05-14 10:10:10 +0430

    add get_username

--- person.cpp
+++ person.cpp
@@ -2,10 +2,18 @@
 
 Person::Person(Input input, int _id)
 {
+	if(input.info[USERNAME] == "" || input.info[PASSWORD] == ""
+		|| input.info[EMAIL] == "" || input.info[AGE])
+		throw Bad_request();
 	info.username = input.info[USERNAME];
 	info.password = input.info[PASSWORD];
 	info.email = input.info[EMAIL];
 	info.age = stoi(input.info[AGE]);
 	info.is_publisher = false;
 	id = _id; 
+}
+
+void Person::get_username()
+{
+	return username;
 }
--- person.h
+++ person.h
@@ -3,6 +3,9 @@
 
 #include "refrence.h"
 #include "film.h"
+#include "bad_request.h"
+#include "not_found.h"
+#include "‫‪permission‬‬_denied‬.h‬"
 
 class Person
 {
@@ -13,6 +16,7 @@ private:
 	int money;
 public:
 	person(Input input, int _id);
+	string get_username();
 	~person();
 	
 };

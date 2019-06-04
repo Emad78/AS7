f25955896561449d108bd00cd77ca5b1d1c76b55
Emad78 <mehdimohajeri78@gmail.com>
2019-06-04 18:25:20 +0430

    update

--- main.cpp
+++ main.cpp
@@ -16,18 +16,17 @@ int main(int argc, char **argv) {
     server.get("/login", new ShowPage("static/login.html"));
     server.post("/login", new LoginHandler(&system));
     server.get("/signup", new ShowPage("static/signup.html"));
-    server.post("/signup", new SignupHandler());
+    server.post("/signup", new SignupHandler(&system));
     server.post("/house", new HouseHandler());    
-    server.get("/house", new ShowPage("static/house.html"));
+    server.get("/house", new HousetHandler(&system, "static/house.html"));
     server.get("/add_film", new ShowPage("static/add_film.html"));        
-    server.post("/add_film", new AddfilmHandler());
+    server.post("/add_film", new AddfilmHandler(&system));
     server.get("/film_details", new ShowPage("static/film_details.html"));        
     server.post("/afilm_details", new FilmdetailsHandler());
     server.get("/profile", new ShowPage("static/profile.html"));            
-    server.post("/profile", new ProfileHandler());    
+    server.post("/profile", new ProfileHandler(&system));    
     server.get("/home.png", new ShowImage("static/home.png"));
     server.get("/", new ShowPage("static/home.html"));
-    cout<<1<<endl;
     server.run();
   } catch (Server::Exception& e) {
     cerr << e.getMessage() << endl;
